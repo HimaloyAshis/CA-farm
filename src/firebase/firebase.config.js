d5de047491a37ccd0a2fcdeb8631c09997356c40
himaloyash <himaloyashis98@gmail.com>
2023-10-16 00:22:34 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbDliq3nsz7I5tCngqSjWOCBb31D5jVP8",
+  authDomain: "ca-farm-a2287.firebaseapp.com",
+  projectId: "ca-farm-a2287",
+  storageBucket: "ca-farm-a2287.appspot.com",
+  messagingSenderId: "704980061377",
+  appId: "1:704980061377:web:e9a8c9d1ece3ce3aa49840"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
